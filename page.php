222be0ebb3042ab24fc69a1ddbc037db0c29446d
IrinaB <balkarovairina@gmail.com>
2026-04-25 05:28:43 +0200

    add menus for parent and children pages

--- page.php
+++ page.php
@@ -5,7 +5,8 @@ get_header();
 while (have_posts()) {
     the_post(); ?>
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title(); ?></h1>
             <div class="page-banner__intro">
@@ -15,23 +16,40 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-<!--        <div class="metabox metabox--position-up metabox--with-home-link">-->
-<!--            <p>-->
-<!--                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About-->
-<!--                    Us</a> <span class="metabox__main">Our History</span>-->
-<!--            </p>-->
-<!--        </div>-->
-<!---->
-<!--        <div class="page-links">-->
-<!--            <h2 class="page-links__title"><a href="#">About Us</a></h2>-->
-<!--            <ul class="min-list">-->
-<!--                <li class="current_page_item"><a href="#">Our History</a></li>-->
-<!--                <li><a href="#">Our Goals</a></li>-->
-<!--            </ul>-->
-<!--        </div>-->
+
+        <?php $theParentId = wp_get_post_parent_id(get_the_ID()); ?>
+
+        <?php if ($theParentId): ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentId) ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i>
+                        Back to <?php echo get_the_title($theParentId) ?>
+                    </a> <span class="metabox__main"><?php the_title(); ?></span>
+                </p>
+            </div>
+        <?php endif ?>
+
+
+        <?php $isParent = get_pages(array('child_of' => get_the_ID())) ?>
+        <?php if ($theParentId or $isParent): ?>
+            <div class="page-links">
+                <h2 class="page-links__title"><a
+                            href="<?php echo get_permalink($theParentId) ?>"><?php echo get_the_title($theParentId) ?></a>
+                </h2>
+                <ul class="min-list">
+                    <?php if ($theParentId): ?>
+                        <?php $findChildrenOf = $theParentId ?>
+                    <?php else: ?>
+                        <?php $findChildrenOf = get_the_ID() ?>
+                    <?php endif; ?>
+                    <?php wp_list_pages(array('title_li' => null, 'child_of' => $findChildrenOf, 'sort_column' => 'menu_order')) ?>
+                </ul>
+            </div>
+        <?php endif; ?>
 
         <div class="generic-content">
-           <?php the_content(); ?>
+            <?php the_content(); ?>
         </div>
     </div>
 <?php }
