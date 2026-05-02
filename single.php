d6de9e0fe7df83e838aa5e45ef4caa27b96db1c1
IrinaB <balkarovairina@gmail.com>
2026-05-02 09:53:20 +0200

    add child-parent pages logic

--- single.php
+++ single.php
@@ -1,14 +1,10 @@
-<?php
+<?php get_header(); ?>
 
-get_header();
-
-while (have_posts()) {
+<?php while (have_posts()):
     the_post(); ?>
+    <h1>Single Post</h1>
     <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
+<?php endwhile; ?>
 
-<?php }
-
-get_footer();
-
-?>
+<?php get_footer(); ?>
