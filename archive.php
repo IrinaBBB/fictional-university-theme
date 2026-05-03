5fcf10f351e47eccfaaa05537ee0d454b83f53c0
IrinaB <balkarovairina@gmail.com>
2026-05-03 12:07:08 +0200

    add blog post and archive templates
    Rename `index.php` to `front-page.php` for the static home page.
    Introduce a new `index.php` for the main blog post listing and `archive.php` for category/author archives.
    Display dynamic recent blog posts on `front-page.php`.
    Update header navigation to highlight active blog and about us pages.
    Standardize the layout of `single.php`.

--- archive.php
+++ archive.php
@@ -3,9 +3,14 @@
     <div class="page-banner__bg-image"
          style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Welcome to our blog!</h1>
+        <h1 class="page-banner__title">
+            <?php the_archive_title(); ?>
+
+            <!--            --><?php //if (is_category()) single_cat_title(); ?>
+            <!--            --><?php //if (is_author()) echo 'Posts by ' . get_queried_object()->display_name; ?>
+        </h1>
         <div class="page-banner__intro">
-            <p>Keep up with our latest news</p>
+            <p><?php the_archive_description(); ?></p>
         </div>
     </div>
 </div>
@@ -20,7 +25,8 @@
                 </a>
             </h2>
             <div class="metabox">
-                <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('j.n.y'); ?> in <?php echo get_the_category_list(', ') ?></p>
+                <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('j.n.y'); ?>
+                    in <?php echo get_the_category_list(', ') ?></p>
             </div>
             <div class="generic-content">
                 <?php the_excerpt(); ?>
--- single.php
+++ single.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 
-<?php while (have_posts()):
-    the_post(); ?>
-    <h1>Single Post</h1>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-<?php endwhile; ?>
+
+<div class="container container--narrow page-section">
+    <?php while (have_posts()):
+        the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+</div>
 
 <?php get_footer(); ?>
