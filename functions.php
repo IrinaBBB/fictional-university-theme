f0584b1e8f964a4d43cb3027561f9afcd1f17399
IrinaB <balkarovairina@gmail.com>
2026-05-04 09:37:20 +0200

    refactor page banner and event summary components
    Introduces a `pageBanner()` function in `functions.php` to centralize the logic for displaying page banners, reducing code duplication across all templates (`archive`, `page`, `single`, `archive-program`, `index`, `page-past-events`, `single-professor`, `single-program`, `single`).
    
    Extracts the event summary display into a reusable `template-parts/content-event.php`, now utilized by `archive-event.php`, `front-page.php`, `page-past-events.php`, and `single-program.php`.
    
    This refactoring significantly improves maintainability and promotes a more modular codebase.

--- functions.php
+++ functions.php
@@ -1,5 +1,44 @@
 <?php
+function pageBanner($args = [])
+{
+    $title = $args['title'] ?? get_the_title();
+    $subtitle = $args['subtitle'] ?? get_field('page_banner_subtitle');
 
+    if (isset($args['photo'])) {
+        $bgImageUrl = $args['photo'];
+    } else {
+        $bgImage = get_field('page_banner_background_image');
+
+        if ($bgImage && !is_archive() && !is_home()) {
+            $bgImageUrl = $bgImage['sizes']['pageBanner'];
+        } else {
+            $bgImageUrl = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
+    ?>
+
+    <div class="page-banner">
+        <div
+                class="page-banner__bg-image"
+                style="background-image: url('<?php echo esc_url($bgImageUrl); ?>')">
+        </div>
+
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title">
+                <?php echo esc_html($title); ?>
+            </h1>
+
+            <?php if ($subtitle): ?>
+                <div class="page-banner__intro">
+                    <p><?php echo esc_html($subtitle); ?></p>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+
+<?php } ?>
+
+<?php
 function university_files()
 {
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
