6010af38ad9581ab2864dc31596d210e63df98a7
IrinaB <balkarovairina@gmail.com>
2026-05-05 12:56:08 +0200

    add front-end elements for search

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import "../css/style.scss"
 import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
 import OSMMap from "./modules/OSMMap"
+import Search from "./modules/Search";
 
 new MobileMenu()
 new HeroSlider()
 new OSMMap()
+new Search()
